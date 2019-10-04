86f98dfbfbb540e20a589e4f6cbca6c795014db7
Mauricio Coba <mauriciocoba@MacBook-Pro-de-Mauricio.local>
2019-10-04 13:45:26 -0300

    Heroku var config

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if (  process.env.NODE_ENV === 'dev'  ) {
     urlDB = 'mongodb://localhost:27017/coffee';
 } else{
-    urlDB = 'mongodb://mcoba:Mauri1983%$#@ds151853.mlab.com:51853/cafe';
+    urlDB = process.env.MONGO_URI;
 }
  
 process.env.URLDB = urlDB;
